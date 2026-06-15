03e1b38af488d8f2d2feb361aa16ee1b6035de37
Santofer <aminebenboubker@gmail.com>
2026-06-15 20:10:53 +0100

    Update Remindian cask to v5.24.7

--- Casks/remindian.rb
+++ Casks/remindian.rb
@@ -1,6 +1,6 @@
 cask "remindian" do
-  version "5.24.6"
-  sha256 "11336ebaf613cb467275a5e85377d17ef43f3b5f0ae013cad88ce32c6476e980"
+  version "5.24.7"
+  sha256 "67c2aba923de1c6be2dd06047122af75f1c2f012514edbace5740fdb23e39f59"
 
   url "https://github.com/Santofer/Remindian/releases/download/v#{version}/Remindian-v#{version}.dmg"
   name "Remindian"
