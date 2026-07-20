04599e89868be835927860878e3c0a33d82dc69a
Santofer <aminebenboubker@gmail.com>
2026-07-20 09:48:50 +0100

    Update Remindian cask to v5.25.13

--- Casks/remindian.rb
+++ Casks/remindian.rb
@@ -2,7 +2,7 @@ cask "remindian" do
   version "5.25.13"
   sha256 "3a3fbdf98fb3ce812a2cfc97c837c77e438dc74fa2fcdeef7e58f6a53d0fd724"
 
-  url "https://github.com/Santofer/Remindian/releases/download/v#{version}/Remindian-v#{version}.dmg"
+  url "https://github.com/Santofer/Remindian/releases/download/v#{version}/Remindian-#{version}.dmg"
   name "Remindian"
   desc "Sync Obsidian tasks with Apple Reminders, Todoist, Asana, Linear, and more"
   homepage "https://github.com/Santofer/Remindian"
